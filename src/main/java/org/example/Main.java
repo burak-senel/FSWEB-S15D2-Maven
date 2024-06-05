2cadc09248d2002f559154890d393a3ad2ae1902
burak-senel <buurak.senel@gmail.com>
2024-06-05 23:07:41 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.entity.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringSet.findUniqueWords();
     }
 }
